a8f7da63fb7045f804be8b2536616616ed9ebc8e
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-26 15:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function manipulateData(inputArray) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(inputArray);
+    }, 3000);
+  })
+  .then(array => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const evenNumbers = array.filter(num => num % 2 === 0);
+        document.getElementById('output').innerText = evenNumbers.join(', ');
+        resolve(evenNumbers);
+      }, 1000);
+    });
+  })
+  .then(evenNumbers => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const multipliedNumbers = evenNumbers.map(num => num * 2);
+        document.getElementById('output').innerText = multipliedNumbers.join(', ');
+        resolve(multipliedNumbers);
+      }, 2000);
+    });
+  });
+}
